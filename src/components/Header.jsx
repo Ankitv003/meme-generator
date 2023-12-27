2df56097eb136cbd4699d0aff326aeeda58266d2
Ankit Verma <ankitv78389@gmail.com>
2023-12-27 11:36:03 +0530

    :tada:[feat][initial commit]

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h3 className="header--project">Created with love💕 - Ankit Verma</h3>
+    </header>
+  );
+}
